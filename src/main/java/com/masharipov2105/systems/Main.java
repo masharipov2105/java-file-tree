64bb870d5a8302730b1702a8013740133c3fba15
masharipov2105 <masharipov2105@gmail.com>
2026-09-18 07:41:54 -0400

    A utility class named TreeGenerator was created.

--- src/main/java/com/masharipov2105/systems/Main.java
+++ src/main/java/com/masharipov2105/systems/Main.java
@@ -4,5 +4,6 @@ public class Main {
     
     public static void main(String[] args) {
     
+        System.out.println(TreeGenerator.generate("/home/masharipov2105/Documents/o'rganish/java/console-calculator-app", false, 0));
     }
 }
---
+++ src/main/java/com/masharipov2105/systems/util/TreeGenerator.java
@@ -0,0 +1,117 @@
+package com.masharipov2105.systems;
+
+import java.util.ArrayList;
+import java.io.File;
+
+public final class TreeGenerator{
+
+	private TreeGenerator(){
+
+		throw new UnsupportedOperationException("Utility class cannot be instantiated!");
+	}
+
+	public static String generate(String path, boolean isChild, int level){
+
+		String tabs = "";
+
+        for (int i = 0; i < level; i ++){
+
+            for (int j = 0; j < 4; j ++){
+
+                tabs += " ";
+            }
+        }
+
+        String finalResult = "";
+
+        if (path == null || path.trim().isEmpty()){
+
+            return "invalid path: path is null";
+        }
+
+	    File file = new File(path);
+
+	    if (!file.exists()){
+
+	        return "Invalid path: path is not exists";
+        }
+
+        if (file.isFile()){
+
+            return "Invalid path: is not folder";
+        }
+
+        if (file == null){
+
+            return "invalid file: file is null";
+        }
+
+        File[] mass = file.listFiles();
+
+        ArrayList<File> mass2 = new ArrayList<>();
+
+        for (File f : mass){
+
+            if (!f.getName().startsWith(".")){
+
+                mass2.add(f);
+            }
+        }
+
+
+        if (!isChild){
+
+            finalResult += (file.getName() + "/\n");
+        }
+
+        if (mass2.size() == 0 && !isChild){
+
+            return file.getName() + "/";
+        }
+
+        for (int i = 0; i < mass2.size(); i ++){
+            if ((mass2.size() - 1) == i){
+
+                finalResult += "└── ";
+
+                if (mass2.get(i).isFile()){
+
+                    finalResult += mass2.get(i).getName() + "\n";
+                } else{
+
+                    finalResult += mass2.get(i).getName() + "/\n";
+
+                    String rekursivResult = generate((path + "/" + mass2.get(i).getName()), true, (level + 1));
+                    String[] lines = rekursivResult.split("\n");
+                    int size_ = lines.length;
+
+                    for (int k = 0; k < size_; k ++){
+
+                        finalResult += ("    " + lines[k] + "\n");
+                    }
+
+                }
+            } else{
+
+                finalResult += "├── ";
+
+                if (mass2.get(i).isFile()){
+
+                    finalResult += mass2.get(i).getName() + "\n";
+                } else{
+
+                    finalResult += mass2.get(i).getName() + "/\n";
+                    String rekursivResult = generate((path + "/" + mass2.get(i).getName()), true, (level + 1));
+                    String[] lines = rekursivResult.split("\n");
+                    int size_ = lines.length;
+
+                    for (int k = 0; k < size_; k ++){
+
+                        finalResult += ("│   " + lines[k] + "\n");
+                    }
+                }
+            }
+        }
+        return finalResult;
+	}
+}
